715ba5314a9c053a90886bd166b6a03c5af189df
joshkokatnur <jkokatnur@gmail.com>
2023-11-28 02:17:24 -0500

    Initial commit

--- src/factory.ts
+++ src/factory.ts
@@ -1,82 +1,30 @@
-import {
-  PairCreated as PairCreatedEvent,
-  SetAuction as SetAuctionEvent,
-  SetAuctionSetter as SetAuctionSetterEvent,
-  SetFeeTo as SetFeeToEvent,
-  SetFeeToSetter as SetFeeToSetterEvent
-} from "../generated/Factory/Factory"
-import {
-  PairCreated,
-  SetAuction,
-  SetAuctionSetter,
-  SetFeeTo,
-  SetFeeToSetter
-} from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts"
+import { PairCreated } from "../generated/Factory/Factory"
+import { Sync as SyncEvent } from "../generated/templates/Pool/Pool"
+import { Pool, Sync } from "../generated/schema"
+import { DataSourceTemplate } from "@graphprotocol/graph-ts"
 
-export function handlePairCreated(event: PairCreatedEvent): void {
-  let entity = new PairCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.token0 = event.params.token0
-  entity.token1 = event.params.token1
-  entity.pair = event.params.pair
-  entity.param3 = event.params.param3
+export function handlePairCreated(event: PairCreated): void {
+  let poolId = event.params.pair.toHexString()
+  let pool = new Pool(poolId)
+  pool.save()
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  DataSourceTemplate.create("Pool", [poolId])
 }
 
-export function handleSetAuction(event: SetAuctionEvent): void {
-  let entity = new SetAuction(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.auction = event.params.auction
+export function handleSync(event: SyncEvent): void {
+  let poolId = event.address.toHex()
+  let pool = Pool.load(poolId)
+  if (pool == null) {
+    pool = new Pool(poolId)
+    pool.save()
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetAuctionSetter(event: SetAuctionSetterEvent): void {
-  let entity = new SetAuctionSetter(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.auctionSetter = event.params.auctionSetter
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetFeeTo(event: SetFeeToEvent): void {
-  let entity = new SetFeeTo(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.feeTo = event.params.feeTo
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetFeeToSetter(event: SetFeeToSetterEvent): void {
-  let entity = new SetFeeToSetter(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.feeToSetter = event.params.feeToSetter
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let syncId = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let sync = new Sync(syncId)
+  sync.pool = pool.id
+  sync.blockTimestamp = event.block.timestamp
+  sync.reserve0 = event.params.reserve0
+  sync.reserve1 = event.params.reserve1
+  sync.save()
 }
